1e13e2b47ddd50f404094e0aca3147a46dc72331
luis <luis@casta.dev>
2024-11-25 23:44:03 -0600

    working with icons

--- prefs.js
+++ prefs.js
@@ -48,14 +48,47 @@ export default class GlaunchPreferences extends ExtensionPreferences {
 
 		// get visible apps
 		const apps = this.getVisibleApps();
-		const model = Gtk.StringList.new(
-			apps.map(app => app.get_display_name())
-		);
+
+		// create a factory for the icons
+		const factory = new Gtk.SignalListItemFactory();
+		factory.connect('setup', (factory, item) => {
+			const box = new Gtk.Box({
+				spacing: 10,
+				orientation: Gtk.Orientation.HORIZONTAL,
+			});
+			const icon = new Gtk.Image({
+				pixel_size: 16
+			});
+			const label = new Gtk.Label({
+				xalign: 0
+			});
+
+			box.append(icon);
+			box.append(label);
+			item.set_child(box);
+		});
+
+		factory.connect('bind', (factory, item) => {
+			const box = item.get_child();
+			const icon = box.get_first_child();
+			const label = icon.get_next_sibling();
+			const app = apps[item.get_position()];
+
+			icon.set_from_gicon(app.get_icon());
+			label.set_label(app.get_display_name());
+		});
+
 
 		const dropDown = new Gtk.DropDown({
-			model: model,
+			model: new Gtk.StringList({
+				strings: apps.map(app => app.get_display_name())
+			}),
+			factory: factory,
 			valign: Gtk.Align.CENTER,
 		});
+
+		// stores the array of AppInfo objects as a custom property
+		dropDown._apps = apps;
 
 		row.add_suffix(dropDown);
 		group.add(row)
